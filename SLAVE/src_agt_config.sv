af50d749ef37d8d52762fa3bbd6d168456e3a0a7
raghuvardhan356 <raghuvardhanchowdam@gmail.com>
2025-05-11 11:33:52 +0530

    Delete SLAVE directory

--- SLAVE/src_agt_config.sv
+++
@@ -1,8 +0,0 @@
-class src_agt_config extends uvm_object;
- virtual router_intf vif;
- bit is_active;
- `uvm_object_utils(src_agt_config)
- function new(string name="src_agt_config");
-  super.new(name);
- endfunction
-endclass
